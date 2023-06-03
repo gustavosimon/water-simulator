c7689ed3970032fe8c67fa6f58815040ee8f9d43
Gustavo Simon <gustavo.andre.simon@hotmail.com>
2023-06-03 15:46:08 -0300

    Implementados os requisitos 2 e 4

--- water_simulator.erl
+++ water_simulator.erl
@@ -1,5 +1,5 @@
 -module(water_simulator).
--export([start/0, generate_molecule/0, handle_hydrogen/0, handle_oxygen/0]).
+-export([start/0, generate_molecule/0, handle_hydrogen/0, handle_oxygen/0, get_energy/0]).
 
 start() -> 
     generate_molecule().
@@ -8,25 +8,30 @@ generate_molecule() ->
     timer:sleep(1000),
     RandomNumber = rand:uniform(2),
     case RandomNumber of
-        1 -> spawn(?MODULE, handle_hydrogen, []);
-        2 -> spawn(?MODULE, handle_oxygen, [])
+        1 -> Pid = spawn(?MODULE, handle_hydrogen, []),
+            io:format("Gerado um Hidrogenio de ID ~p~n", [Pid]);
+        2 -> Pid = spawn(?MODULE, handle_oxygen, []),
+            io:format("Gerado um Oxigenio de ID ~p~n", [Pid])
     end,
     generate_molecule().
 
 handle_hydrogen() ->
-    io:format("Gerado um Hidrogenio~n"),
+    get_energy(),
     ok.
 
 handle_oxygen() ->
-    io:format("Gerado um Oxigenio~n"),
+    get_energy(),
     ok.
 
-% Criar uma função para gerar moléculas aleatoriamente (cada molécula deve um novo processo Erlang)
-%  A função de geração de moléculas deve ser constante e aleatória a cada X tempo
-% Criar um controle para verificar se as moléculas já estão prontas para serem utilizadas
+get_energy() -> 
+    Min = 10000,
+    Max = 30000,
+    RandomValue = rand:uniform(Max - Min + 1) + Min,
+    timer:sleep(RandomValue).
+
 
 % 1. Cada molécula gerada, hidrogênio e oxigênio deve ser um processo em Erlang; ok
-% 2. O tempo para que cada molécula adquira energia suficiente deve variar entre 10s e 30s;
+% 2. O tempo para que cada molécula adquira energia suficiente deve variar entre 10s e 30s; ok
 % 3. A geração de moléculas deve ser constante e de forma aleatória com intervalo de tempo parametrizável; ok
-% 4. Cada processo deve ser identificado unicamente e apresentar uma mensagem quando criado informando esta identificação;
+% 4. Cada processo deve ser identificado unicamente e apresentar uma mensagem quando criado informando esta identificação; ok
 % 5. A aplicação deve identificar as combinações realizadas, apresentando a identificação dos elementos combinados;
